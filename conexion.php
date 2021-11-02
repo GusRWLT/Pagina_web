16ca74e3986800116765197cbd5e7c33039d3c9c
Gustavo <86806523+GusRWLT@users.noreply.github.com>
2021-11-02 04:34:30 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,14 +1,13 @@
 <?php
-    $server = "localhost";                           //Servidor donde está alojada la BD
-    $user = "root";                                  //Nombre de usuario de la BD
-    $pass_db = "";                                   //Contraseña de la BD
-    $database = "donar";                             //Nombre de la BD
+    $hostname = "localhost";                           //Servidor donde está alojada la BD
+    $username = "root";                                //Nombre de usuario de la BD
+    $password = "";                                    //Contraseña de la BD
+    $database = "donar";                               //Nombre de la BD
 
-    $conexion = new mysqli($server, $user, $pass_db, $database);       //Establece la conexión con la DB
+    $conexion = new mysqli($hostname, $root, $password, $database);       //Establece la conexión con la DB
     if ($conexion->connect_errno) {                                 //Comprueba si hubo algún error al conectarse
         echo "Fallo al conectar a la base de datos: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
-        //header("Location:error_db.php");                            //Si se produjo un error, redirije a una página de error
-        exit();
+        exit();     //Finaliza la ejecución del programa
     }
     $conexion->set_charset("utf8");     //Establece el estandar de codificaciones para los caracteres de la DB (es para que se vean bien las tildes y ñ)
 ?>
